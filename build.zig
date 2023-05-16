e18d0a730f5121280228d3eaa7e1f347cbfbb408
mlarouche <michael.larouche@gmail.com>
2023-05-16 08:47:46 -0400

    Update to latest Zig

--- build.zig
+++ build.zig
@@ -29,9 +29,9 @@ pub fn build(b: *std.build.Builder) void {
     exe.addModule("zigimg", zigimg_dep.module("zigimg"));
     exe.addModule("sdl2", sdk.getWrapperModule());
     sdk.link(exe, .dynamic);
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
